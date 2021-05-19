825475d473900c71f6d4195f441aeb046ac4a677
HackStrix <hackstrix99@gmail.com>
2021-05-19 18:59:27 +0530

    Completing the blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,19 +1,18 @@
 '''
 Author : Sankalp Narula
-Date : 19-05-2021
+Date : 19-05-2021 Format (dd-mm-yyyy)
 Time: 12:56pm IST
 
 '''
 # importing the dependent libraries
+from flask import Flask, app,jsonify
 import datetime
 import hashlib
 import json
-from flask import Flask,jsonify
 
 # classes
 
 class Blockchain():
-
 
     def __init__(self) -> None:
         self.chain = []
@@ -29,3 +28,78 @@ class Blockchain():
                 }
         self.chain.append(block)
         return block
+
+    def getpreviousblock(self):
+        return self.chain[-1]
+
+    def pow(self, previous_proof): # proof of work
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[0:4] == '0000':
+                check_proof = True
+            else:
+                new_proof+=1
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def isChainValid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[0:4] != '0000':
+                return False
+            previous_block = block
+
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+blockchain_obj = Blockchain()
+
+@app.route('/mineblock', methods=["GET"])
+
+def mine_block():
+    previous_block = blockchain_obj.getpreviousblock()
+    previous_proof = previous_block['proof']
+    proof = blockchain_obj.pow(previous_proof)
+    previous_hash = blockchain_obj.hash(previous_block)
+    block = blockchain_obj.create_block(proof, previous_hash)
+    response = {
+                'message': 'Congratulation, you just mined a block!',
+                'index':block['index'],
+                'timestamp':block['timestamp'],
+                'proof':block['proof'],
+                'previous_hash':block['previous_hash']
+                }
+    return jsonify(response), 200
+
+
+# getting the full blockchain
+@app.route('/get_chain',methods = ['GET'])
+
+def get_chain():
+    response = {
+                'chain': blockchain_obj.chain,
+                'length': len(blockchain_obj.chain)
+                }
+    return jsonify(response), 200
+
+app.run(host='0.0.0.0',port=5000)
